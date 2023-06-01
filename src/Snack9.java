f10fb58e6e30a6f4d1f10b7ef482b7f698b0c496
LucaDalRi <euro2xd@gmail.com>
2023-06-01 15:55:05 +0200

    Esercizio 9 completato

--- src/Snack9.java
+++ src/Snack9.java
@@ -1,5 +1,22 @@
 public class Snack9 {
     public static void main(String[] args) {
+//      Calcola la somma e la media dei primi 10 numeri.
+
+        int[] arrNum = {1,2,3,4,5,6,7,8,9,10};
+
+        int numSum = 0;
+        int numAvg;
+
+        for (int i = 0; i < arrNum.length; i++) {
+
+            numSum += arrNum[i];
+
+        }
+
+        numAvg = numSum / arrNum.length;
+
+        System.out.println("Somma dei numeri -> " + numSum);
+        System.out.println("Media dei numeri -> " + numAvg);
 
     }
 }
