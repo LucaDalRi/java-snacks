0ad8ffaf48a0458d3ee162836d0ab1640fdb3bf3
LucaDalRi <euro2xd@gmail.com>
2023-06-01 15:09:05 +0200

    Esercizio 2 completato

--- src/Snack3.java
+++ src/Snack3.java
@@ -1,5 +1,22 @@
 public class Snack3 {
     public static void main(String[] args) {
+//      Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+        int[] arrInt = {1,3,12,14,56,54,31,12,22};
+
+        int total = 0;
+
+        for (int i = 0; i < arrInt.length; i++) {
+
+            if (i % 2 != 0) {
+
+                total += arrInt[i];
+
+            }
+
+        }
+
+        System.out.println(total);
 
     }
 }
